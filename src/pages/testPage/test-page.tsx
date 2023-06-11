873f678601725011ad47ab978d6b195391e07d84
luvlaceeeee <nikitan2002@gmail.com>
2023-06-11 16:58:14 +0300

    feat: add fetch to hook

--- src/pages/testPage/test-page.tsx
+++ src/pages/testPage/test-page.tsx
@@ -3,15 +3,15 @@ import { useTestMock } from '@/shared/hooks';
 import { useState } from 'react';
 
 export function TestPage() {
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<any[]>([]);
 
-  useTestMock();
+  useTestMock(setCount);
 
   return (
     <div className="flex flex-col items-center justify-between space-y-3">
       <h1 className="text-2xl font-bold">Boilerplate setup</h1>
-      <Button onClick={() => setCount((already) => already + 1)}>
-        count is {count}
+      <Button onClick={() => console.log(count[1].title)}>
+        count is {count.length}
       </Button>
     </div>
   );
--- src/shared/hooks/useTestMock.ts
+++ src/shared/hooks/useTestMock.ts
@@ -1,11 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { API_URL } from '../lib';
 import { useEffect } from 'react';
 
-export const useTestMock = () => {
+export const useTestMock = (setter: React.Dispatch<any>) => {
   useEffect(() => {
     fetch(`${API_URL}/books`)
       .then((res) => res.json())
       // eslint-disable-next-line no-console
-      .then((data) => console.log(data));
+      .then((data) => {
+        setter(data);
+      });
   }, []);
 };
