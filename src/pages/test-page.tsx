0588e9a87667df292536ef11c717afad72455f67
luvlaceeeee <nikitan2002@gmail.com>
2023-06-13 00:28:54 +0300

    fix: change test page

--- src/pages/test-page.tsx
+++ src/pages/test-page.tsx
@@ -3,10 +3,8 @@ import { useLocation } from 'react-router-dom';
 export function TestPage() {
   const location = useLocation().pathname;
   return (
-    <div className="flex flex-col items-center justify-between space-y-3">
-      <h1 className="text-2xl font-bold">
-        Это тестовая страница :) вот ее путь {location}
-      </h1>
+    <div className="flex flex-col justify-between space-y-3">
+      <h1 className="text-4xl font-medium">{location}</h1>
     </div>
   );
 }
