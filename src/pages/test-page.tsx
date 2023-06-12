c105c55f99abace615ae45d96bc8beedfa90ccb8
luvlaceeeee <nikitan2002@gmail.com>
2023-06-13 00:02:17 +0300

    fix: delete empty export and change test-page

--- src/pages/test-page.tsx
+++ src/pages/test-page.tsx
@@ -5,7 +5,7 @@ export function TestPage() {
   return (
     <div className="flex flex-col items-center justify-between space-y-3">
       <h1 className="text-2xl font-bold">
-        Это тестовая страница :) вот ее пусть {location}
+        Это тестовая страница :) вот ее путь {location}
       </h1>
     </div>
   );
