ba21200a3876be27f5c25c90c3ac9031450d60c3
Бондарева Анастасия <seltaria@mail.ru>
2023-06-13 13:36:42 +0300

    feat: add user store file

---
+++ src/shared/state/user.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const user = createSlice({
+  name: 'user',
+  initialState: {
+    id: 1,
+    name: 'Кирилл',
+    surname: 'Черных',
+    email: 'mail@chernix.com',
+    password: '123456',
+    imageUrl:
+      'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  },
+  reducers: {
+    changeData: (state, action) => {
+      return {
+        ...state,
+        name: action.payload.name,
+        surname: action.payload.surname,
+        email: action.payload.email,
+        imageUrl: action.payload.imageUrl,
+        password: action.payload.password,
+      };
+    },
+  },
+});
+
+export const { changeData } = user.actions;
+export default user;
