0e56b5b52220d57f30ff1131000be8914dbb31b0
luvlaceeeee <nikitan2002@gmail.com>
2023-06-14 01:58:34 +0300

    refactor: add collabsible to sidebar

--- src/features/sidebar/components/sidebar-button.tsx
+++ src/features/sidebar/components/sidebar-button.tsx
@@ -1,12 +1,12 @@
 import { Button, Separator } from '@/shared/components';
-import { ArrowRight, Plus } from 'lucide-react';
+import { ArrowRight, ChevronsUpDown, Plus } from 'lucide-react';
 import React, { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 export interface SidebarButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
-  path: string;
+  path?: string;
   icon: 'plus' | 'arrow';
 }
 
@@ -25,20 +25,27 @@ export const SidebarButton: FC<SidebarButtonProps> = ({
         asChild
         size="lg"
         variant="ghost"
-        className={`flex w-full justify-between px-3 text-accent-foreground/50 hover:bg-transparent ${
+        className={`flex w-full justify-between px-3 hover:bg-transparent ${
           location === path
             ? 'text-accent-foreground'
             : 'text-accent-foreground/50'
         }`}
       >
-        <Link to={path}>
-          {title}
-          {icon === 'plus' ? (
-            <Plus className="h-4 w-4" />
-          ) : (
-            <ArrowRight className="h-4 w-4" />
-          )}
-        </Link>
+        {path ? (
+          <Link to={path}>
+            {title}
+            {icon === 'plus' ? (
+              <Plus className="h-4 w-4" />
+            ) : (
+              <ArrowRight className="h-4 w-4" />
+            )}
+          </Link>
+        ) : (
+          <div>
+            {title}
+            <ChevronsUpDown className="h-4 w-4" />
+          </div>
+        )}
       </Button>
       <Separator />
     </>
--- src/features/sidebar/components/sidebar.tsx
+++ src/features/sidebar/components/sidebar.tsx
@@ -1,20 +1,43 @@
-import { sidebarButtons } from '../data/sidebarButtons';
+import { sidebarButtons, sidebarButtonsSearch } from '../data/sidebarButtons';
 import { SidebarButton } from './sidebar-button';
-import { Separator } from '@/shared/components';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/shared/components';
 import { generateKey } from '@/shared/utils';
+import { useState } from 'react';
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="w-1/6 pr-7">
-      <Separator />
-      {sidebarButtons.map((button) => (
-        <SidebarButton
-          key={generateKey(button.title)}
-          icon={button.icon}
-          title={button.title}
-          path={button.path}
-        />
-      ))}
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <div>
+          {sidebarButtons.map((button) => (
+            <SidebarButton
+              key={generateKey(button.title)}
+              icon={button.icon}
+              title={button.title}
+              path={button.path}
+            />
+          ))}
+          <CollapsibleTrigger asChild>
+            <SidebarButton icon="arrow" title="Поиск по:" />
+          </CollapsibleTrigger>
+        </div>
+        <CollapsibleContent>
+          {sidebarButtonsSearch.map((button) => (
+            <SidebarButton
+              key={generateKey(button.title)}
+              icon={button.icon}
+              title={button.title}
+              path={button.path}
+            />
+          ))}
+        </CollapsibleContent>
+      </Collapsible>
     </div>
   );
 };
