b043d4b56cc8f801e845139e53931df19d49120b
luvlaceeeee <nikitan2002@gmail.com>
2023-06-13 23:08:34 +0300

    feat: add button to sidebar

--- src/features/sidebar/data/sidebarButtons.ts
+++ src/features/sidebar/data/sidebarButtons.ts
@@ -3,6 +3,7 @@ import { SidebarButtonProps } from '../components/sidebar-button';
 // TODO Добавить в путь кнопки профиля userId из будущего стора
 
 export const sidebarButtons: SidebarButtonProps[] = [
+  { title: 'Мои книги', path: '/books', icon: 'arrow' },
   { title: 'Добавить книгу', path: '/books/add_book', icon: 'plus' },
   { title: 'Жанры', path: '/books/genre', icon: 'arrow' },
   { title: 'Авторы', path: '/books/author', icon: 'arrow' },
