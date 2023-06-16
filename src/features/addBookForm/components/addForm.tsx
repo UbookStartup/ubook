5880a6d875203958be47cf76144c8b06989db2b4
AndrewGanzha <ganzha-2002@mail.ru>
2023-06-16 16:15:32 +0300

    feat: add some styles for form

--- src/features/addBookForm/components/addForm.tsx
+++ src/features/addBookForm/components/addForm.tsx
@@ -17,45 +17,56 @@ export const AddForm = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...(register('author'),
-          {
-            required: true,
-            placeholder: 'Имя автора',
-          })}
-          type="text"
-        />
-        <select>
-          <option selected disabled>
-            Выберите жанр
-          </option>
-        </select>
-        <input
-          {...(register('title'),
-          {
-            required: true,
-            placeholder: 'Название книги',
-          })}
-          type="text"
-        />
-        <input
-          {...(register('image'),
-          {
-            required: false,
-          })}
-          type="file"
-        />
-        <input
-          {...(register('rating'),
-          {
-            required: true,
-          })}
-          type="range"
-          min="1"
-          max="5"
-        />
-        <button>Send</button>
+      <form
+        className="flex flex-col gap-16 text-base"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex justify-between">
+          <input
+            {...(register('title'),
+            {
+              required: true,
+              placeholder: 'Название книги',
+            })}
+            type="text"
+          />
+          <select>
+            <option selected disabled>
+              Выберите жанр
+            </option>
+          </select>
+        </div>
+        <div className="flex justify-between">
+          <input
+            {...(register('author'),
+            {
+              required: true,
+              placeholder: 'Имя автора',
+            })}
+            type="text"
+          />
+          <input
+            {...(register('rating'),
+            {
+              required: true,
+            })}
+            type="range"
+            min="1"
+            max="5"
+          />
+        </div>
+        <div className="flex justify-between">
+          <input
+            {...(register('image'),
+            {
+              required: false,
+            })}
+            type="file"
+          />
+          <button className="w-80 bg-gray-200 py-4 font-bold">
+            Добавить книгу
+          </button>
+        </div>
       </form>
     </div>
   );
