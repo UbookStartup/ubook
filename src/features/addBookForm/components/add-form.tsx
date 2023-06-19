5014d1dae4e370bbe2b7b882e8d2f760f1fdfb8e
luvlaceeeee <nikitan2002@gmail.com>
2023-06-20 01:01:27 +0300

    feat: add rating to form

--- src/features/addBookForm/components/add-form.tsx
+++ src/features/addBookForm/components/add-form.tsx
@@ -1,6 +1,8 @@
-import { Button, Input, Separator } from '@/shared/components';
+import { Button, Input, Rating, Separator } from '@/shared/components';
+import { useState } from 'react';
 
 export const AddForm = () => {
+  const [rating, setRating] = useState(0);
   return (
     <div className="flex flex-col gap-4">
       <h1 className="mr-2 text-4xl font-medium">Добавление книги</h1>
@@ -33,11 +35,7 @@ export const AddForm = () => {
           </div>
           <div className="space-y-1">
             <label htmlFor="name">Оценка</label>
-            <Input
-              type="text"
-              id="name"
-              className="rounded-none border-b border-input"
-            />
+            <Rating rating={rating} setRating={setRating} />
           </div>
           <div>
             <Button variant="text" type="button" className="pl-0">
