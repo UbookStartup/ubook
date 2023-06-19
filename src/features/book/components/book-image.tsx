7c6dc0e5ee95ce4b2a608b9b7ab47b4ebdde4d8c
luvlaceeeee <nikitan2002@gmail.com>
2023-06-20 02:51:59 +0300

    feat: create add image form for book page

--- src/features/book/components/book-image.tsx
+++ src/features/book/components/book-image.tsx
@@ -1,8 +1,11 @@
-import { Button } from '@/shared/components';
-import { FC, useContext } from 'react';
+import { Button, Input } from '@/shared/components';
+import { useContext, useState } from 'react';
+import { useUpdateBookMutation } from '../service/book.api';
 import { BookContext } from './book';
 
-export const BookImage: FC = () => {
+export const BookImage = () => {
+  const [isInputOpen, setIsInputOpen] = useState(false);
+
   const { image, title } = useContext(BookContext);
   return (
     <div className="relative h-96 w-72 shrink-0 bg-secondary-foreground/20">
@@ -20,11 +23,56 @@ export const BookImage: FC = () => {
           <Button
             variant="secondary"
             className="absolute bottom-0 w-full rounded-none"
+            onClick={() => setIsInputOpen(!isInputOpen)}
           >
-            Загрузить фото
+            {isInputOpen ? 'Закрыть форму' : 'Загрузить фото'}
           </Button>
         </>
       )}
+      {isInputOpen && <BookImage.AddForm />}
+    </div>
+  );
+};
+
+BookImage.AddForm = function BookImageAddForm(): JSX.Element {
+  const [inputType, setInputType] = useState<'text' | 'file'>('text');
+  const [inputValue, setInputValue] = useState('');
+
+  const { id } = useContext(BookContext);
+  const [updateBook, { isSuccess }] = useUpdateBookMutation();
+
+  const submitHandler = () => {
+    updateBook({ id, image: inputValue });
+  };
+  return (
+    <div className="absolute -bottom-28 w-full">
+      <Input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        className="mb-3 rounded-none border file:text-primary"
+        type={inputType}
+        placeholder="Введите ссылку"
+      />
+      <div className="flex items-center justify-between">
+        <Button
+          variant="text"
+          className="px-0"
+          onClick={() => {
+            setInputValue('');
+            setInputType(inputType === 'text' ? 'file' : 'text');
+          }}
+        >
+          {inputType === 'file' ? 'Вставить ссылку' : 'Загрузить файл'}
+        </Button>
+        <Button
+          variant="secondary"
+          size="sm"
+          disabled={!inputValue}
+          onClick={submitHandler}
+        >
+          Загрузить
+        </Button>
+      </div>
     </div>
   );
 };
